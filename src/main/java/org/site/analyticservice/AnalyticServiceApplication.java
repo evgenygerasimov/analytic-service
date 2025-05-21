319ab952d70d1d7452de6a5b8434e8665a24ccaa
evgen <evgenygerasimoff@gmail.com>
2025-05-21 10:34:26 +0400

    REF code;

--- src/main/java/org/site/analyticservice/AnalyticServiceApplication.java
+++ src/main/java/org/site/analyticservice/AnalyticServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnalyticServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnalyticServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticServiceApplication.class, args);
+    }
 
 }
